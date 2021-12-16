65ec129c048ced3c01bff7fffb97636efa3d92c5
Sunny Kumar <sunnykumar@bunnycodec.com>
2021-12-16 11:49:32 +0530

    Remain upload

--- execute/run.sh
+++ execute/run.sh
@@ -1,20 +1,12 @@
 # To run this shell script from terminal type 'sh run.sh'
 # Compulosory line below
 cd ..
-# cd Reports/
-# rm -rf *
-# cd ..
-# cd Logs/
-# rm -rf *
-# cd Screenshots/
-# rm -rf *
-# cd ..
 
 # Uncomment any one of the line
 # pytest -v -s -m "sanity or regression" --html=Reports/reports_chrome.html --browser chrome
 # pytest -v -s -m "sanity and regression" --html=Reports/reports_chrome.html --browser chrome
 # pytest -v -s -m "regression" --html=Reports/reports_chrome.html --browser chrome
-pytest -v -s -m "sanity" --html=Reports/reports_chrome.html --browser chrome
+# pytest -v -s -m "sanity" --html=Reports/reports_chrome.html --browser chrome
 
 # pytest -v -s -m "sanity or regression" --html=Reports/reports_chrome.html --browser firefox
 # pytest -v -s -m "sanity and regression" --html=Reports/reports_chrome.html --browser firefox
